3c068a0ef9e7868e19642ac57a69dd725dce7a2f
rajitchadar59 <rajitathiya123@gmail.com>
2026-07-24 15:58:14 +0530

    change backend url

--- bargit-admin/src/environment.js
+++ bargit-admin/src/environment.js
@@ -1,5 +1,5 @@
-let IS_PROD=false;
+let IS_PROD=true;
 
-const server= IS_PROD ?"https://bargit.com/api" : "http://localhost:5000/api"
+const server= IS_PROD ?"https://bargit-backend.onrender.com" : "http://localhost:5000/api"
 
 export default server;
